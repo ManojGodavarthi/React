ef1d4f689be4d64c53d84420d3fa8907564765da
ManojGodavarthi <95137801+ManojGodavarthi@users.noreply.github.com>
2022-07-24 12:03:10 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+import pi from "./math.js";
+
+//what to show,where to show it
+ReactDom.render(
+  <div>
+    <h1>Hello!</h1>
+    <p>Hai i am manoj</p>
+  </div>,
+  document.getElementById("root")
+);
